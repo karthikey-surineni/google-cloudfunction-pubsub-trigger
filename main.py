15b1a9270d6a3a9c78ad568c0bf1143895efef28
Karthikey Surineni <karthikey.surineni@auspost.com.au>
2019-11-19 01:41:53 +1100

    fix

--- main.py
+++ main.py
@@ -38,38 +38,5 @@ def cf_pubsub_trigger(event, context):
         payload = json.loads(name)
         response = run_build_trigger(payload)
     else:
-        response = 'No data'
-    print('Response {response}'.format(response))
-
-def hello_error_1(request):
-    # [START functions_helloworld_error]
-    # This WILL be reported to Stackdriver Error
-    # Reporting, and WILL NOT show up in logs or
-    # terminate the function.
-    from google.cloud import error_reporting
-    client = error_reporting.Client()
-
-    try:
-        raise RuntimeError('I failed you')
-    except RuntimeError:
-        client.report_exception()
-
-    # This WILL be reported to Stackdriver Error Reporting,
-    # and WILL terminate the function
-    raise RuntimeError('I failed you')
-
-    # [END functions_helloworld_error]
-
-def hello_error_2(request):
-    # [START functions_helloworld_error]
-    # WILL NOT be reported to Stackdriver Error Reporting, but will show up
-    # in logs
-    import logging
-    print(RuntimeError('I failed you (print to stdout)'))
-    logging.warn(RuntimeError('I failed you (logging.warn)'))
-    logging.error(RuntimeError('I failed you (logging.error)'))
-    sys.stderr.write('I failed you (sys.stderr.write)\n')
-    # This WILL be reported to Stackdriver Error Reporting
-    from flask import abort
-    return abort(500)
-    # [END functions_helloworld_error]
+        response = {}
+    print('Response {response}'.format(response=response))
